b7b02ac08edd6a49e24c7ad6abd992ca2774608d
akhilaPuppala <akhilandeshwariv@gmail.com>
2025-01-09 15:03:42 +0530

    deloy

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO movie_details (movie_title, movie_language, movie_release_date, movie_image_url)
+VALUES 
+    ('The Shawshank Redemption', 'English', '1994-09-22', 'https://example.com/shawshank.jpg'),
+    ('The Dark Knight', 'English', '2008-07-18', 'https://example.com/darkknight.jpg'),
+    ('Inception', 'English', '2010-07-16', 'https://example.com/inception.jpg'),
+    ('Parasite', 'Korean', '2019-05-30', 'https://example.com/parasite.jpg'),
+    ('Spirited Away', 'Japanese', '2001-07-20', 'https://example.com/spiritedaway.jpg');
